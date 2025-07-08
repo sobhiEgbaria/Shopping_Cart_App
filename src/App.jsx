f241b0b202ecb4ddf260128d52860428e817983a
sobhiEgbaria <sobhim686@gmail.com>
2025-07-08 10:57:20 +0300

    header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
+import Header from "./components/Header";
+import Products from "./components/Products";
+import ShoppingCart from "./components/ShoppingCart";
 function App() {
-  return <>gogo lolo say hi</>;
+  return (
+    <div class="flex flex-col gap-4 text-white p-6">
+      <Header />
+      <div class="flex flex-col md:flex-row justify-between gap-4">
+        <Products />
+        <ShoppingCart />
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+const Header = () => {
+  return (
+    <div class="flex justify-between items-center p-6 bg-gray-800 text-white rounded-lg">
+      <h1 class="text-2xl font-bold">Shopping Cart</h1>
+      <p class="bg-amber-600 text-white text-lg font-semibold py-1 px-4 rounded-full">
+        0 Items
+      </p>
+    </div>
+  );
+};
+
+export default Header;
